8d23ed7c4e5c35eb0fbb68473a2fa8e0a5b41906
frelion <frelionsun@gmail.com>
2026-07-23 17:14:03 +0800

    fix(release): match Bun package links by suffix

--- scripts/build-binaries.sh
+++ scripts/build-binaries.sh
@@ -122,11 +122,9 @@ if [[ "$SKIP_DEPS" == "false" ]]; then
         local link_parent="$3"
         local scope="${package_name%%/*}"
         local name="${package_name#*/}"
-        local package_dir="$(pwd)/node_modules/$scope/$name"
-        if [[ ! -d "$package_dir" ]]; then
-            package_dir=$(find "$(pwd)/node_modules" \( -type d -o -type l \) -path "*/node_modules/$scope/$name" -print -quit)
-        fi
-        if [[ -z "$package_dir" || ! -d "$package_dir" ]]; then
+        local package_dir
+        package_dir=$(find "$(pwd)/node_modules" \( -type d -o -type l \) -path "*/$scope/$name" -print -quit)
+        if [[ -z "$package_dir" ]]; then
             echo "Could not find installed package ${package_name}@${version}" >&2
             exit 1
         fi
